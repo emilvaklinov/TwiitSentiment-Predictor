ae75bee18b560828ef56e5f6871dff6715bbdd63
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-01-13 11:01:33 +0000

    Revert "swifter added"
    This reverts commit 171282b61db1dcc276255ade50b16f5736247c3e.

--- Twiit-Sentiment/ViewController.swift
+++ Twiit-Sentiment/ViewController.swift
@@ -6,26 +6,16 @@
 //
 
 import UIKit
-import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-    
-    let swifter = Swifter(consumerKey: API KEY, consumerSecret: SECURITY API KEY)
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            print(results)
-        }) { (error) in
-            print("There was an error with Twitter API Request, \(error)")
-        }
-
+        
     }
 
     @IBAction func predictPressed(_ sender: Any) {
